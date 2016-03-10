6cd5bf95df91f84ec42138b4ddd5d8b9e125a42d
Rickx777 <Cwong7@stuy.edu>
2016-03-09 21:58:41 -0500

    Tester coded in.

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -14,14 +14,20 @@ through every element multiple times but not quite n^2. This would make the
 total for the algoritm O(n+nlog(n)) but this is effectivly O(nlog(n)).
 
   Mean execution times for dataset of size n:
-  Batch size: <# of times each dataset size was run>
-  n=1       time: 
-  n=10      time: 
-  n=100     time: 
+  Batch size: <10>
+  n=1       time: 32901.1 nanoseconds 
+  n=10      time: 38941.5 nanoseconds
+  n=100     time: 96679.7 nanoseconds
   ...
-  n=<huge>  time: 
+  n=10000   time: 1.71164 * 10^6 nanoseconds
+  n=100000  time: 1.39364 * 10^7 nanoseconds
+  n=<1000000>  time: 1.40515 * 10^8 nanoseconds
   ANALYSIS:
-  <INSERT YOUR RESULTS ANALYSIS HERE>
+  The sort is extremely efficient. As can be seen with the results, the runtime execution
+  times increase relatively slowly from n sizes from 1 to 100. Then it increases in a 
+  manner that more closely resembles 10s (where s = log(n - 10000) where n >= 10000), as
+  n moves past 10000. This indicates that the runtime increases in a manner faster than
+  O(n) and slower than O(n^2), thereby reinforcing that merge sort runs with O(nlog(n)).
   ======================================*/
 
 public class MergeSortTester 
@@ -34,19 +40,22 @@ public class MergeSortTester
 
     /******************************
      * execution time analysis 
-     * <INSERT YOUR DESCRIPTION HERE OF 
-     *  YOUR APPARATUS FOR GENERATING EXECUTION 
-     *  TIME DATA...>
+     * The testing apparatis uses an int array Franz of various size. It first replaces each
+     * element of the array with with a randomly generated value between 0 and 49. Then it
+     * takes the current value of the system timer in nanoseconds, runs the sort, and adds
+     * the difference of the new value of the system timer and the value before the sort
+     * to a sum. This process is repeated 10 times. Afterwhich, the average of the 10 times
+     * is printed for the user to analyze.
      ******************************/
     public static void main( String[] args ) 
     {
-	int[] Franz = new int[10];
+	int[] Franz = new int[100000];
         double Time = 0;
-        long initial;
+        double initial;
 
-	populate(Franz);
-
+	//Times the merge sort for 10 separate sorts.
         for (int i = 0; i < 10; i++) {
+	     populate(Franz);	     
              initial = System.nanoTime();
              MergeSort.sort(Franz);
              Time += System.nanoTime() - initial;
